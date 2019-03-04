18daaaa0abf2e7fa2e1e45a9ca5b65dea34c62c6
kiki <kiki@bmi-tech.com>
2019-03-04 14:55:21 +0800

    modify SM4 block cipher algorithm in ECB mode, enale SM4 test TODO: pass user key

--- AlgoProcLib/smfourecbcrypt.cpp
+++ AlgoProcLib/smfourecbcrypt.cpp
@@ -14,7 +14,12 @@ SMFourECBCrypt::SMFourECBCrypt(CRPT_TYPE cryptype)
 
 int SMFourECBCrypt::ProcessAlgorithm(AlgorithmParams &param)
 {
-    sms4_key_t *pubkey = NULL;
+    unsigned char key[16] = {
+        0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+        0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
+    };
+
+    sms4_key_t pubkey;
 
     unsigned char inBuf[param.strIn.length()];
     memset(inBuf, 0, sizeof(inBuf));
@@ -23,11 +28,25 @@ int SMFourECBCrypt::ProcessAlgorithm(AlgorithmParams &param)
     unsigned char outBuf[MAX_BUF_SIZE];
     memset(outBuf, 0, MAX_BUF_SIZE);
 
-    sms4_ecb_encrypt(inBuf, outBuf, pubkey, m_crypType);
+    printf("before sm4:\n");
+    for(int i = 0; i < 16; i++)
+        printf("%02x ", inBuf[i]);
+    printf("\n");
+
+    if(m_crypType == CRYP_ENC)
     {
-        fprintf(stderr, "%s() failed to SM4_ECB_Encrypt\n", __func__);
-        return RES_SERVER_ERROR;
+        sms4_set_encrypt_key(&pubkey, key);
     }
+    else
+    {
+        sms4_set_decrypt_key(&pubkey, key);
+    }
+    sms4_ecb_encrypt(inBuf, outBuf, &pubkey, m_crypType);
+
+    printf("after  sm4:\n");
+    for(int i = 0; i < 16; i++)
+        printf("%02x ", outBuf[i]);
+    printf("\n");
 
     param.strOut = string(reinterpret_cast<const char*>(outBuf));
     return RES_OK;
