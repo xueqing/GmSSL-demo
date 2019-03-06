61c1c1a76eb4cd5a762036b890bf41f8353c8937
kiki <kiki@bmi-tech.com>
2019-03-06 15:05:47 +0800

    add SM4 implement using EVP_* interface TODO: SM4_ECB decryption failure

--- AlgoProcLib/smfourecbcrypt.cpp
+++ AlgoProcLib/smfourecbcrypt.cpp
@@ -1,7 +1,18 @@
 #include "smfourecbcrypt.h"
 
+#include <string.h>
+
+#define SM4_EVP 0
+
+#if SM4_EVP
 #include <openssl/evp.h>
+#include <openssl/err.h>
+#include <crypto/evp/evp_locl.h>
+#include <openssl/ossl_typ.h>
+#include <crypto/internal/evp_int.h>
+#else
 #include <openssl/sms4.h>
+#endif
 
 using namespace std;
 using namespace GB;
@@ -14,6 +25,62 @@ SMFourECBCrypt::SMFourECBCrypt(CRPT_TYPE cryptype)
 
 int SMFourECBCrypt::ProcessAlgorithm(AlgorithmParams &param)
 {
+#if SM4_EVP
+    int nret = RES_SERVER_ERROR;
+
+    unsigned char key[16] = {
+        0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+        0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
+    };
+
+    unsigned char inBuf[param.strIn.length()];
+    memset(inBuf, 0, sizeof(inBuf));
+    memcpy(inBuf, param.strIn.c_str(), param.strIn.length());
+
+    unsigned char outBuf[MAX_BUF_SIZE];
+    memset(outBuf, 0, MAX_BUF_SIZE);
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    do
+    {
+        printf("before sm4:\n");
+        for(int i = 0; i < 16; i++)
+            printf("%02x ", inBuf[i]);
+        printf("\n");
+
+        if(ctx == NULL)
+        {
+            fprintf(stderr, "%s() failed to allocate ctx\n", __func__);
+            break;
+        }
+
+        if(EVP_CipherInit(ctx, EVP_sm4_ecb(), key, nullptr, m_crypType) == 0)
+        {
+            ERR_print_errors_fp(stderr);
+            break;
+        }
+
+        int lenOut = 0;
+        if(EVP_CipherUpdate(ctx, outBuf, &lenOut, inBuf, param.strIn.length()) == 0
+                || EVP_CipherFinal(ctx, outBuf, &lenOut) == 0)
+        {
+            ERR_print_errors_fp(stderr);
+            break;
+        }
+
+        printf("after  sm4:\n");
+        for(int i = 0; i < 16; i++)
+            printf("%02x ", outBuf[i]);
+        printf("\n");
+
+        param.lenOut = lenOut;
+        nret = RES_OK;
+    }while(false);
+
+    EVP_CIPHER_CTX_free(ctx);
+    param.strOut = string(reinterpret_cast<const char*>(outBuf));
+    return nret;
+#else
     unsigned char key[16] = {
         0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
         0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
@@ -50,4 +117,5 @@ int SMFourECBCrypt::ProcessAlgorithm(AlgorithmParams &param)
 
     param.strOut = string(reinterpret_cast<const char*>(outBuf));
     return RES_OK;
+#endif
 }
